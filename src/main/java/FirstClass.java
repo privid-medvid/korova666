22c15e79c2ce47c88c632ed3effe49f4761e8305
privid-medvid <DSHEGERA@LOHIKA.COM>
2014-12-05 15:48:19 +0200

    123

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dshegera
+ * Date: 12/5/14
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FirstClass {
+
+    public static main ();
+
+
+
+}
