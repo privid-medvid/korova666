a0661810e4a46165872daa10f35291216c757d78
privid-medvid <DSHEGERA@LOHIKA.COM>
2014-12-16 11:07:30 +0200

    Subsequence
    Example
    Input:
    list1: 1,2,3,4,5,6,7
    list2: 2,4,5;
    Output: true;
    Input:
    list1: 1,2,3,4,5,6,7
    list2: 2,4,3;
    Output: false

---
+++ src/main/java/subsequence/Subsequence.java
@@ -0,0 +1,79 @@
+package subsequence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by dshegera on 12/15/2014
+ */
+
+public class Subsequence {
+
+
+    private static List<Character> superList = new ArrayList<Character>();
+    private static List<Character> subList = new ArrayList<Character>();
+
+    static void getSeqs() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insert superRaw with numbers");
+        String superSequence = scanner.nextLine();
+
+        System.out.print("Insert subRaw with numbers");
+        String subSequence = scanner.nextLine();
+
+        for (int i = 0; i < superSequence.length(); i++) {
+            if (Character.isDigit(superSequence.charAt(i))) {
+                superList.add(superSequence.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < subSequence.length(); i++) {
+            if (Character.isDigit(subSequence.charAt(i))) {
+                subList.add(subSequence.charAt(i));
+            }
+        }
+    }
+
+
+    private static void algorythm() {
+        int beginOfScanOfSup = 0;
+        boolean ifWasSubSeq = false;
+
+        for (Character sub : subList) {
+
+            for (int i = beginOfScanOfSup; i < superList.size(); i++) {
+                if (Character.getNumericValue(sub) == Character.getNumericValue(superList.get(i))) {
+                    beginOfScanOfSup = i++;
+                    ifWasSubSeq = true;
+                    break;
+                }
+                ifWasSubSeq = false;
+            }
+
+
+            if (ifWasSubSeq == false) {
+                break;
+            }
+        }
+
+        if (ifWasSubSeq == true) {
+            System.out.println("Yes, it is a subsequence");
+        } else {
+            System.out.println("BoolShit");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        getSeqs();
+        algorythm();
+
+    }
+}
+
+
+
